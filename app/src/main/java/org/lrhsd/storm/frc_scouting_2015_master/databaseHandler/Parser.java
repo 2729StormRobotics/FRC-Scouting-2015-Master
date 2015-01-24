f37e54ee5fb4976cff72d5cbaef13789d52ed677
Matthew Rassmann <matt.rassmann@me.com>
2015-01-24 15:18:00 -0500

    Added splitting up the matches

--- app/src/main/java/org/lrhsd/storm/frc_scouting_2015_master/databaseHandler/Parser.java
+++ app/src/main/java/org/lrhsd/storm/frc_scouting_2015_master/databaseHandler/Parser.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 import org.lrhsd.storm.frc_scouting_2015_master.MainActivity;
 
+import java.util.ArrayList;
+
 import me.dm7.barcodescanner.zbar.Result;
 
 
@@ -15,12 +17,26 @@ public class Parser {
 
     public static void Parse(Result result, Activity activity) {
         String input = result.getContents();
-        Log.d("Parser input", input);
+        //Log.d("Parser input", input);
         if (!input.contains("@stormscouting")) {
             Intent i = new Intent(activity, MainActivity.class);
             activity.startActivity(i);
             Toast.makeText((Context) activity.getApplicationContext(), (CharSequence) "Scan a Valid QR Code", Toast.LENGTH_LONG).show();
             return;
         }
+        input = input.substring(input.indexOf(" ")+1);
+        ArrayList<String> teams = new ArrayList<String>();
+        int i = 0;
+        while(input.contains(":")){
+            String temp;
+            temp=input.substring(0,input.indexOf(":"));
+            input=input.substring(input.indexOf(":")+1);
+            teams.add(i,temp);
+            Log.d("arrayList"+i,teams.get(i));
+            i++;
+        }
+
+
+        //Log.d("input","1"+input);
     }
 }
