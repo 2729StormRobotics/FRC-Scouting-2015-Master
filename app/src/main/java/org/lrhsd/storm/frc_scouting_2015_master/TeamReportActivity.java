6c0cc6dc5fbf5577adf5362519ad5af8bf2136ee
GarrettBeatty <gman9732@gmail.com>
2015-02-15 14:19:52 -0500

    changed layout

--- app/src/main/java/org/lrhsd/storm/frc_scouting_2015_master/TeamReportActivity.java
+++ app/src/main/java/org/lrhsd/storm/frc_scouting_2015_master/TeamReportActivity.java
@@ -1,13 +1,10 @@
 package org.lrhsd.storm.frc_scouting_2015_master;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ScrollView;
 import android.widget.Spinner;
@@ -24,29 +21,6 @@ public class TeamReportActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_report_layout);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_team_report, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void addToScrollView(TeamData team,String teamNum,int matchNum){
